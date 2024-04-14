c2d0b0595c5cfe5e93d5fac4d2fa635ffd43ecf7
SerafinaChen2 <167019904+SerafinaChen2@users.noreply.github.com>
2024-04-14 19:16:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+import numpy as np
 
+pd.set_option('display.max_columns', None)
+
+# Set page configuration
+st.set_page_config(
+    page_title="Spotify Mania",
+    page_icon="https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+# Enable dark theme for Altair
+alt.themes.enable("dark")
