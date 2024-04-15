7133636dc69407ac08d5e4ed2dc37fa858f98b23
SerafinaChen2 <167019904+SerafinaChen2@users.noreply.github.com>
2024-04-15 20:09:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -185,7 +185,7 @@ scatter = scatter_base.mark_point().encode(
 # Bar plot for musical characteristics
 bar_base = alt.Chart(filtered_data).properties(width=800, height=100)
 bars = bar_base.mark_bar().encode(
-    x=alt.X('percentage:Q', title='Average Percentage'),
+    x=alt.X('Percentage:Q', title='Average Percentage'),
     y=alt.Y('Musical Characteristic:N', title='Musical Characteristics'),
     color=alt.Color('Musical Characteristic:N',
                     scale=alt.Scale(domain=['Danceability', 'Valence', 'Energy'],
