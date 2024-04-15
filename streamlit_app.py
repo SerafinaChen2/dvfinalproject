ab02ba5a20d2d4aa819db0d60cf8cc2f3d8b2b91
SerafinaChen2 <167019904+SerafinaChen2@users.noreply.github.com>
2024-04-15 21:48:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ st.subheader("Top tracks plot and Radar plot with Attributes")
 st.sidebar.title("Filter Options")
 
 # Create dropdown widgets for start_date and end_date
-start_date = st.sidebar.selectbox("Start Date", spotify['release_date'].min())
-end_date = st.sidebar.selectbox("End Date", spotify['release_date'].max())
+start_date = st.sidebar.("Start Date", spotify['release_date'].min())
+end_date = st.sidebar.("End Date", spotify['release_date'].max())
 
 
 # Convert start_date and end_date to datetime for filtering
