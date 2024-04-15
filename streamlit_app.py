961a0f0312a1b0f27f2565aaded5192678fd2f11
SerafinaChen2 <167019904+SerafinaChen2@users.noreply.github.com>
2024-04-15 05:23:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 from math import pi
 
@@ -13,7 +14,7 @@ st.set_page_config(
 
 # Load Spotify data
 spotify = pd.read_csv('spotify-2023.csv', encoding='ISO-8859-1')
-spotify['release_date'] = pd.to_datetime(spotify['released_year'].astype(str) + '-' + spotify['released_month'].astype(str).str.zfill(2))
+spotify['release_date'] = pd.to_datetime(spotify['released_year'].astype(str) + '-' + spotify['released_month'].astype(str).str.zfill(2), errors='coerce')
 
 # Sidebar for filtering options
 st.sidebar.title("Filter Options")
@@ -21,8 +22,6 @@ start_date = st.sidebar.date_input("Start Date", spotify['release_date'].min())
 end_date = st.sidebar.date_input("End Date", spotify['release_date'].max())
 
 # Convert dates and filter data
-start_date = pd.to_datetime(start_date)
-end_date = pd.to_datetime(end_date)
 filtered_data = spotify[(spotify['release_date'] >= start_date) & (spotify['release_date'] <= end_date)]
 
 # Function to create a bar plot
@@ -34,7 +33,7 @@ def create_top_songs_bar_plot(data, top_n):
     ax.set_xticklabels(top_songs['track_name'], rotation=90)
     ax.set_ylabel("Streams")
     ax.set_title("Top Songs Based on Number of Streams")
-    return fig
+    return top_songs, fig
 
 # Radar chart function
 def create_radar_chart(data, attributes):
@@ -57,8 +56,8 @@ def create_radar_chart(data, attributes):
 # User selections from sidebar
 top_n_options = [10, 20, 50, 100]
 selected_top_n = st.sidebar.selectbox("Select number of top songs to display:", top_n_options)
-top_songs_data = create_top_songs_bar_plot(filtered_data, selected_top_n)
-st.pyplot(top_songs_data)
+top_songs_data, top_songs_plot = create_top_songs_bar_plot(filtered_data, selected_top_n)
+st.pyplot(top_songs_plot)
 
 # Calculate mean audio features and create radar chart
 mean_audio_features = top_songs_data[['danceability_%', 'valence_%', 'energy_%', 'acousticness_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%']].mean().tolist()
